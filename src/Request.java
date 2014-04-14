157d32bab606677fe6cff78eec891388bc4b7812
llaki <llaki10@freeuni.edu.ge>
2014-04-14 04:07:03 +0400

    Added Security Code Stuff
    We're generating a security code for each phone number that's gonna use
    our application. Then we're sending it to the user as a message and
    he/she should type that security code inside the application together
    with the phone number to authenticate.

--- src/Request.java
+++ src/Request.java
@@ -7,73 +7,103 @@ public class Request {
 		ArrayList<String> ids = req.getMatchIds();
 		System.out.println(ids);
 	}
-	
-	private String phoneNumber;
-	
+
+	private String phoneNumber, securityCode, numberWithCode;
+
 	private ArrayList<String> matchIds;
-	
-	public Request(String requestString){
+
+	public Request(String requestString) {
 		matchIds = new ArrayList<String>();
 		int index = requestString.indexOf("[");
-		if(index==-1){
-			phoneNumber = removeSpacesBothSides(requestString);
+		if (index == -1) {
+			numberWithCode = removeSpacesBothSides(requestString);
 			return;
 		}
-		phoneNumber = removeAllSpaces(removeSpacesBothSides(requestString.substring(0, index)));
+		numberWithCode = removeAllSpaces(removeSpacesBothSides(requestString
+				.substring(0, index)));
+		phoneNumber = extractPhoneNumber(numberWithCode);
+		securityCode = extractSecurityCode(numberWithCode);
+		if(!CodeGenerator.validatePair(phoneNumber, securityCode)){
+			// the pair is invalid, so we just stop here
+			return;
+		}
 		User u = new User(phoneNumber);
-		if(phoneNumber==null || phoneNumber.length()==0){
+		if (phoneNumber == null || phoneNumber.length() == 0) {
 			u = User.TEST_USER;
 		}
-		for(int i=index; i<requestString.length(); i++){
-			if(requestString.charAt(i)!='[') continue;
+		for (int i = index; i < requestString.length(); i++) {
+			if (requestString.charAt(i) != '[')
+				continue;
 			int till = i;
-			for(int j=i+1; j<requestString.length(); j++){
-				if(requestString.charAt(j)==']') break;
-				else till = j;
+			for (int j = i + 1; j < requestString.length(); j++) {
+				if (requestString.charAt(j) == ']')
+					break;
+				else
+					till = j;
 			}
-			String matchId = requestString.substring(i+1, till+1);
+			String matchId = requestString.substring(i + 1, till + 1);
 			matchIds.add(matchId);
-			MatchUserBinder.addMatchToUser(u, AllGoingMatches.getExistingMatchObject(matchId));
+			MatchUserBinder.addMatchToUser(u,
+					AllGoingMatches.getExistingMatchObject(matchId));
 			i = till;
 		}
-		
+
 	}
-	
-	private static String removeAllSpaces(String text){
+
+	private static String extractPhoneNumber(String numberAndCode) {
+		if (numberAndCode.length() == 0)
+			return "";
+		int index = numberAndCode.indexOf(DIVIDER);
+		if (index == -1)
+			return numberAndCode;
+		return numberAndCode.substring(0, index);
+	}
+
+	private static String extractSecurityCode(String numberAndCode) {
+		if (numberAndCode.length() == 0)
+			return "";
+		int index = numberAndCode.indexOf(DIVIDER);
+		if (index == numberAndCode.length() - 1 || index == -1)
+			return "";
+		return numberAndCode.substring(index + 1);
+	}
+
+	private static String removeAllSpaces(String text) {
 		String result = "";
-		for(int i=0; i<text.length(); i++){
-			if(Character.isDigit(text.charAt(i)) || text.charAt(i)=='+')
+		for (int i = 0; i < text.length(); i++) {
+			if (Character.isDigit(text.charAt(i)) || text.charAt(i) == '+')
 				result += text.charAt(i);
 		}
 		return result;
 	}
-	
-	public ArrayList<String> getMatchIds(){
+
+	public ArrayList<String> getMatchIds() {
 		return matchIds;
 	}
-	
-	public String getPhoneNumber(){
+
+	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	
-	private static String removeSpacesBothSides(String text){
+
+	private static String removeSpacesBothSides(String text) {
 		int min = -1, max = text.length();
-		for(int i=0; i<text.length(); i++){
-			if(text.charAt(i)==' '){
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == ' ') {
 				min = i;
-			}
-			else break;
+			} else
+				break;
 		}
-		for(int i=text.length()-1; i>=0; i--){
-			if(text.charAt(i)==' '){
+		for (int i = text.length() - 1; i >= 0; i--) {
+			if (text.charAt(i) == ' ') {
 				max = i;
-			}
-			else break;
+			} else
+				break;
 		}
-		if(min>max) return "";
-		return text.substring(min+1, max);
+		if (min > max)
+			return "";
+		return text.substring(min + 1, max);
 	}
-	
-	
-	
+
+	private static final char DIVIDER = '&';
+
 }
